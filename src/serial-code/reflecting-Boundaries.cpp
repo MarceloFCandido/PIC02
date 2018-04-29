78736ea55bab499923e8dc7ec26084936c64be44
Marcelo Ferreira Cândido <marmacedoo9@gmail.com>
2018-04-29 16:45:51 -0300

    Finalizando as classes do reflecting-Boundaries É necessário testar o funcionamento das classes e criar métodos de serialização para os dados a serem recebidos e passados para frente

--- src/serial-code/reflecting-Boundaries.cpp
+++ src/serial-code/reflecting-Boundaries.cpp
@@ -6,6 +6,15 @@
 
 using namespace std;
 
+/*
+ *    This program aims to solve the bidimensional wave equation 
+ *    by the finite differences method. This methods will be im-
+ *    plemented with the help of Eigen library, that provides the
+ *    necessary tools of the Linear Algebra, mainly the vectors 
+ *    and matrices thats will be used to store and manipulate data, 
+ *    as well the methods that are necessary to this.
+ */
+
 # define PI 3.141592653589793238463
 
 // Defining my own Array type object
@@ -97,15 +106,23 @@ class velocity {
             return this->c;
         }
 
+        double getGradientVelocity(double x, double y) {
+            // TODO: document
+            return this->a * x + this->b * y + this->c;
+        }
+
 };
 
-class Onda2D {
+class _2DWave {
+    /**
+     * TODO: document
+     */ 
     private:
         double Lx, Ly, tMax, Mx, Ny, w, A, Xp, Yp, Tp; // Entries by the user
         double dx, dy, dt, Ot, R; // Method's internal variables
 
     public:
-        Onda2D (double Lx, 
+        _2DWave (double Lx, 
                 double Ly, 
                 double tMax, 
                 double Mx, 
@@ -115,6 +132,7 @@ class Onda2D {
                 double Xp, 
                 double Yp, 
                 double Tp) {
+            // TODO: document
             this->Lx = Lx;       // Extension of medium in x
             this->Ly = Ly;       // Depth of the medium in y
             this->tMax = tMax;   // Maximum simulation time
@@ -138,7 +156,7 @@ class Onda2D {
         RowArrayIJ evaluateFXYT(RowArrayIJ X, RowArrayIJ Y, RowArrayIJ T) {
             /**
              * Function that returns a bidimensional velocities array.
-             * TODO: terminar a documentacao
+             * TODO: document
             */
             
             // Defining Tterm
@@ -167,7 +185,15 @@ class Onda2D {
 
         }
 
-        RowArrayIJ getVelocityMatrix(vector<interface> interfaces, vector<velocity> velocities) {
+        RowArrayIJ getVelocitiesMatrix(vector<interface> interfaces, vector<velocity> velocities) {
+            /** Function that returns the bidimensional velocities matrix of the medium.
+             *  For each medium's point that the wave propagates, we calculate a velocity based
+             *  on which layer of the medium this point is found.
+             * 
+             *  Receives:       interfaces - a vector of interface objects
+             *                  velocities - a vector of velocity objects, where each object 
+             *                               represents the velocity function of its respective layer
+             */ 
             // Creating iterators
             vector<interface> :: iterator ifs;
             vector<velocity> :: iterator vls;
@@ -176,32 +202,33 @@ class Onda2D {
             RowArrayIJ v((int) this->Mx, (int) this->Ny);
 
             double x, y;
-
+            int k = 0;
+            // Putting the values on the velocities matrix
             for (int i = 0; i < this->Mx; i++) {
-                x = i * this->dx;
+                x = i * this->dx;   // An step in the abscissas
                 for (int j = 0; j < this->Ny; j++) {
-                    y = j * this->dy;
-                    
+                    y = j * this->dy;   // An step in the ordinates
+                    while (y > interfaces[k].getY(x) && k < interfaces.size() - 1) {
+                            k += 1; // Looking for on which layer the point is 
+                    }
+                    // Calculating the velocity in the point
+                    v(i, j) = velocities[k].getGradientVelocity(x, y);
                 }
             }
+
+            return v;
 
         }
 };
 
-/*
- *    This program aims to solve the bidimensional wave equation 
- *    by the finite differences method. This methods will be im-
- *    plemented with the help of Eigen library, that provides the
- *    necessary tools of the Linear Algebra, mainly the vectors 
- *    and matrices thats will be used to store and manipulate data, 
- *    as well the methods that are necessary to this.
- */
-
 int main () {
 
-    RowArrayIJ a(5, 10);
-
-    cout << a;
+    double Lx = 5.;
+    double Ly = 5.;
+    double tMax = 30.;
+    int Mx = 50;
+    int Ny = 50;
+    _2DWave w();
 
     return 0;
 
