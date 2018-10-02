d5afe5e0bb7fd65d6162e3df7edb94a000cd22b8
Marcelo Ferreira Cândido <marmacedoo9@gmail.com>
2018-10-01 22:08:56 -0300

    Continuando a criar a exibição do resultado do código fake Há dificuldade em encontrar função assimétricas

--- src/fake-code/function-solver.cpp
+++ src/fake-code/function-solver.cpp
@@ -8,15 +8,15 @@ using namespace arma;
 
 int main(int argc, char const *argv[]) {
 
+    int x_points, y_points;
+    float x_ofst, y_ofst;
+    float x, y;
+
     // Creating objects for conversion of arguments
     stringstream convert0(argv[1]);
     stringstream convert1(argv[2]);
     stringstream convert2(argv[3]);
     stringstream convert3(argv[4]);
-
-    int x_points, y_points;
-    float x_ofst, y_ofst;
-    float x, y;
 
     // Putting arguments on variables
     convert0 >> x_points;
@@ -24,12 +24,17 @@ int main(int argc, char const *argv[]) {
     convert2 >> y_points;
     convert3 >> y_ofst;
 
-    cout << "X points: " << argv[1] << "\n";
-    cout << "X offset: " << argv[2] << "\n";
-    cout << "Y points: " << argv[3] << "\n";
-    cout << "Y offset: " << argv[4] << "\n";
+    cout << "X points: " << x_points << "\n";
+    cout << "X offset: " << x_ofst << "\n";
+    cout << "Y points: " << y_points << "\n";
+    cout << "Y offset: " << y_ofst << "\n";
 
     mat A(x_points, y_points);
+    rowvec parameters(4);
+
+    // Storing parameters in a vector for a file
+    parameters(0) = x_points; parameters(1) = x_ofst;
+    parameters(2) = y_points; parameters(3) = y_ofst;
 
     // Calculating function
     double start = omp_get_wtime();
@@ -46,7 +51,7 @@ int main(int argc, char const *argv[]) {
 
     printf("Elapsed time: %f\n", end - start);
 
-    
+    parameters.save("pmts.dat", raw_ascii);
     A.save("A.dat", raw_binary);
 
     return 0;
--- src/fake-code/function-viewer.py
+++ src/fake-code/function-viewer.py
@@ -1,7 +1,28 @@
 #!/usr/bin/python2.7
 #!-*- coding: utf8 -*-
 
-import numpy as np
-import matplotlib.pyplot as plt
+import numpy as np;
+import matplotlib.pyplot as plt;
 
+# Loading data
 A = np.fromfile('A.dat', dtype=float);
+[x_points, x_ofst, y_points, y_ofst] = np.loadtxt('pmts.dat');
+
+# print x_points;
+# print x_ofst;
+# print y_points;
+# print y_ofst;
+
+# Preparing data for plotting
+X = np.linspace(0., x_points * x_ofst, num=int(x_points));
+Y = np.linspace(0., y_points * y_ofst, num=int(y_points));
+A = A.reshape(int(x_points), int(y_points));
+
+# Preparing plot
+fig, ax = plt.subplots();
+CS = ax.contour(X, Y, A);
+ax.clabel(CS, inline=1, fontsize=10);
+ax.set_title('z = x ^ 3 + y ^ 2');
+
+ax.plot();
+plt.savefig("A.png");
