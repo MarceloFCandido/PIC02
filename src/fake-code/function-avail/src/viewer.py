09963654fb3cce3abdecfba6016c18d5a272c92b
Marcelo Ferreira Cândido <marmacedoo9@gmail.com>
2019-01-21 22:35:26 -0200

    Tentando criar código serial fake de avaliação de função

---
+++ src/fake-code/function-avail/src/viewer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python2.7
+#!-*- coding: utf8 -*-
+
+import numpy as np;
+import matplotlib.pyplot as plt;
+
+# Loading data
+A = np.fromfile('data/outputs/A.dat', dtype=float);
+[x_points, x_ofst, xi, y_points, y_ofst, yi] = np.loadtxt('data/outputs/pmts.dat');
+
+
+# Preparing data for plotting
+X = np.linspace(xi, xi + x_points * x_ofst, num=int(x_points));
+Y = np.linspace(yi, yi + y_points * y_ofst, num=int(y_points));
+A = A.reshape(int(x_points), int(y_points));
+
+# print X.shape;
+# print Y.shape;
+# print A.shape;
+
+[B, C] = np.meshgrid(X, Y)
+
+# Preparing plot
+fig, ax = plt.subplots();
+CS = ax.contourf(B, C, A.transpose(), 20, cmap='RdGy');
+ax.clabel(CS, inline=False, fontsize=10);
+ax.set_title('z = x ^ 3 + y ^ 2');
+
+# plt.xlim(-2, 2);
+# plt.ylim(-2, 2);
+ax.plot();
+plt.savefig("data/images/A.png");
