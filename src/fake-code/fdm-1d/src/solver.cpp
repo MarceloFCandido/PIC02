435e20e28205c9a60b4f2a0265cc432227255d4c
Marcelo Ferreira Cândido <marmacedoo9@gmail.com>
2019-02-20 23:20:31 -0300

    Tentando terminar fake serial fdm-1d Alguma coisa está incompreensível. Talvez a equação esteja errada!

--- src/fake-code/fdm-1d/src/solver.cpp
+++ src/fake-code/fdm-1d/src/solver.cpp
@@ -5,12 +5,26 @@
 using namespace std;
 using namespace arma;
 
+/** solver.cpp
+  * usage: make run 0=a 1=b 2=c 3=d 4=e 5=f 6=g 7=h
+  * where
+  * 0 - x points
+  *	1 - x ofset
+  *	2 - x start
+  * 3 - wave's peak
+  * 4 - time steps
+  * 5 - total time
+  * 6 - wave's peak time
+  * 7 - Amplitude
+  * 8 - R
+**/
+
 #define FRAMES_PER_SECOND 24
 
 // some global variables to be used in the calculations
-float A, R;
+float A, R, t_w/*ave */, x_w/*ave */;
 
-float fxt(float x, float x_w, float t, float t_w) {
+float fxt(float x, float t) {
 	float termT = R * (t - t_w) * (t - t_w);
 	float D = (x - x_w) * (x - x_w) + (t - t_w) * (t - t_w);
 	float termD = R * D;
@@ -22,9 +36,6 @@ int main(int argc, char const *argv[]) {
     int   x_points   , t_points;
     float x_ofst     , t_ofst;
     float x_b/*egin*/, t_t/*otal*/;
-	float x_w/*ave */, t_w/*ave */ ;
-	
-	// TODO: Prepare to receive A and R
 
     // Creating objects for conversion of arguments
     stringstream convert0(argv[1]);
@@ -33,6 +44,8 @@ int main(int argc, char const *argv[]) {
     stringstream convert3(argv[4]);
     stringstream convert4(argv[5]);
     stringstream convert5(argv[6]);
+	stringstream convert6(argv[7]);
+	stringstream convert7(argv[8]);
 
     // Putting arguments on variables
     convert0 >> x_points;
@@ -41,49 +54,46 @@ int main(int argc, char const *argv[]) {
 	convert3 >> x_w;
     convert4 >> t_t;
 	convert5 >> t_w;
+	convert6 >> A;
+	convert7 >> R;
 
     t_points = (int) t_t * FRAMES_PER_SECOND;
 	t_ofst = t_t / t_points;
-	
+
 	cout << "X points:     " << x_points << "\n";
     cout << "X offset:     " << x_ofst   << "\n";
     cout << "T size:       " << t_t      << "\n";
     cout << "T wave's pic: " << t_w      << "\n";
 
-    mat A(x_points, t_points);
+    mat A(t_points, x_points);
     rowvec parameters(5);
 
     // Storing parameters in a vector for a file
-    parameters(0) = x_points; parameters(1) = x_ofst; parameters(2) = x_b;
-    parameters(3) = t_t; parameters(4) = t_points;// parameters(5) = t_t;
-
-    wall_clock timer;
+    parameters(0) = x_points;
+	parameters(1) = x_ofst;
+	parameters(2) = x_b;
+    parameters(3) = t_t;
+	parameters(4) = t_points;
 
     // Calculating function
     float x_i = x_b;
     float t_j = 0.;
-    // timer.tic();
 
     float x_ofst_2 = x_ofst * x_ofst;
     float t_ofst_2 = t_ofst * t_ofst;
     float termA = t_ofst_2 / x_ofst_2;
 
-    for (int i = 1; i < x_points - 1; i++) {
-        for (int j = 1; j < t_points - 1; j++) {
-	    	A(i, j + 1) = termA * (A(i - 1, j) - 2. * A(i, j) + A(i + 1, j)) - \
-	      		A(i, j - 1) + 2. * A(i, j) + fxt(x_i, t_j);
-            t_j += t_ofst;
+    for (int i = 2; i < t_points - 1; i++) {
+        for (int j = 1; j < x_points - 1; j++) {
+	    	A(i + 1, j) = termA * (A(i, j - 1) - 2. * A(i, j) + A(i, j + 1)) - A(i - 1, j) + 2. * A(i, j) + fxt(x_i, t_j);
+            x_i += x_ofst;
         }
-        x_i += x_ofst;     
-        t_j = 0.;
+        t_j += t_ofst;
+        x_i = 0.;
     }
-
-    // double eTime = timer.toc();
-    // printf("Elapsed time: %lf\n", eTime);
 
     parameters.save("data/outputs/pmts.dat", raw_ascii);
     A.save("data/outputs/A.dat", raw_binary);
 
     return 0;
 }
-	
--- src/fake-code/fdm-1d/src/viewer.py
+++ src/fake-code/fdm-1d/src/viewer.py
@@ -12,21 +12,23 @@ A = np.fromfile('data/outputs/A.dat', dtype=float);
 # Preparing data for plotting
 X = np.linspace(xi, xi + x_points * x_ofst, num=int(x_points));
 T = np.linspace(0., tt, num=int(t_points));
-A = A.reshape(int(x_points), int(t_points));
+A = A.reshape(int(t_points), int(x_points));
 
 # print X.shape;
 # print Y.shape;
-# print A.shape;
+print A.shape;
 
 [B, C] = np.meshgrid(X, T)
 
 # Preparing plot
 fig, ax = plt.subplots();
-CS = ax.contourf(B, C, A.transpose(), 20, cmap='RdGy');
-ax.clabel(CS, inline=False, fontsize=10);
 ax.set_title('Wave in a string');
+# CS = ax.contourf(B, C, A, 20, cmap='RdGy');
+# ax.clabel(CS, inline=False, fontsize=10);
+# plt.xlabel('X')
+# plt.ylabel('T')
 
 # plt.xlim(-2, 2);
 # plt.ylim(-2, 2);
-ax.plot();
+ax.plot(A[1500, :]);
 plt.savefig("data/images/A.png");
